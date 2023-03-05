a676ed2be0bdc47ae05dd14d1ef0e579c49cb221
Vansh Arora <vansha10@gmail.com>
2023-03-05 23:43:06 +0530

    Add from message

--- shriya/effect.js
+++ shriya/effect.js
@@ -173,6 +173,7 @@ $('document').ready(function(){
 			if(i==50){
 				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
 					$('.cake').fadeIn('fast');
+					$('.cake-message').fadeIn('fast');
 				});
 				
 			}
